6d5ca64194d1b7d9c3050b650d17deaeba2019af
Lincoln Ferreira <lincolnbf@gmail.com>
2018-08-16 18:59:06 -0300

    Ajustes no Endpoint/categorias/id

--- src/main/java/com/lincolnbf/cursomc/services/CategoriaService.java
+++ src/main/java/com/lincolnbf/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.lincolnbf.cursomc.domain.Categoria;
 import com.lincolnbf.cursomc.repositories.CategoriaRepository;
+import com.lincolnbf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
