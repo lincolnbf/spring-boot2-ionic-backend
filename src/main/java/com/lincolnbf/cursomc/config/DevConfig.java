9c336499d2d6b0265b3a1cc077d8493b16239989
Lincoln Ferreira <lincolnbf@gmail.com>
2018-08-23 14:50:39 -0300

    Implementando SMPT Service com servidor do google

--- src/main/java/com/lincolnbf/cursomc/config/DevConfig.java
+++ src/main/java/com/lincolnbf/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.lincolnbf.cursomc.services.DBService;
+import com.lincolnbf.cursomc.services.EmailService;
+import com.lincolnbf.cursomc.services.SMTPEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		
 		dbService.instantiateDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService () {
+		return new SMTPEmailService();
 	}
 	
 }
--- src/main/java/com/lincolnbf/cursomc/services/DBService.java
+++ src/main/java/com/lincolnbf/cursomc/services/DBService.java
@@ -117,7 +117,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "13158722690", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "lincolnbf@gmail.com", "13158722690", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("32410133", "999827700"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38500-000", cli1, c1);
